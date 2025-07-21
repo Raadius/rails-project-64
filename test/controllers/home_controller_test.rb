65a73597c80014c7a77df8ba3eb2d807f586c5a2
Aleksey Tyurin <a.tyurin@teachbase.ru>
2025-07-21 11:21:57 +0300

    fix home controller test, lint simple form

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
+# TODO[ALX]: Здесь тест должен получить index со списком постов. При попытке на этой странице
+# TODO[ALX]: куда-то тыкнуть, должен перенаправить на страницу входа. Пока тут тест на редирект при открытии рутовой страницы
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get home_index_url
-    assert_response :success
+    get root_url
+    assert_response :redirect
+    assert_redirected_to new_user_session_url
   end
 end
